e871b022762d709ce79a6f5877c9cfcaf5642706
saxatachi <saxatachi@gmail.com>
2021-04-18 21:53:49 +0200

    deploy do heroku

--- trueeffectsbackend/Dockerfile
+++ trueeffectsbackend/Dockerfile
@@ -5,3 +5,4 @@ COPY requirements.txt /code/
 RUN pip install -r requirements.txt
 COPY . /code/
 
+
--- trueeffectsbackend/app/urls.py
+++ trueeffectsbackend/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
+from django.views.generic import TemplateView
 
 urlpatterns=[
     path('display_exercises/',views.exerciseView,name="exercise-view"),
